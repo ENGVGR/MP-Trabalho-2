ebc040faa68f1bb10de405f41d4d4deb712f8297
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-25 18:53:39 -0300

    Implementado a funcao que retorna qual e o menor

--- juncao.py
+++ juncao.py
@@ -3,6 +3,14 @@ Funções para juntar dois arquivos .txt ordenados em um arquivo .txt ordenado
 """
 
 def ler_arquivo(arquivo):
-  with open(arquivo, "r", encoding="utf-8") as arq:
-    linhas = arq.readlines()
+  with open(arquivo, "r", encoding="utf-8") as arquivo_aberto:
+    linhas = arquivo_aberto.readlines()
   return linhas
+
+def menor_valor(valor_1, valor_2):
+  if valor_1 < valor_2:
+    return "Primeiro"
+  elif valor_1 > valor_2:
+    return "Segundo"
+  else:
+    return "Iguais"
