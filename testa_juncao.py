7e30f3240cfd3da41ce37bf43a1f7adf52a2ba66
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-25 18:58:51 -0300

    Teste que verifica se ao fornecer um arquivo em formato de lista e um indice, ele retorna o valor da lista, no indice indicado, como inteiro

--- testa_juncao.py
+++ testa_juncao.py
@@ -4,6 +4,7 @@ Teste para as funções em juncao.py
 
 from juncao import ler_arquivo
 from juncao import menor_valor
+from juncao import string_para_inteiro
 
 def teste_ler_arquivo():
   arquivo_teste = ["1\n", "2\n", "3\n", "4\n", "5"]
@@ -17,3 +18,11 @@ def teste_menor_valor():
   assert menor_valor(8, 2) == "Segundo"
   assert menor_valor(7, 7) == "Iguais"
   assert menor_valor(3, 9) == "Primeiro"
+
+def teste_string_para_inteiro():
+  arquivo_teste = ["1\n", "2\n", "3\n", "4\n", "5"]
+  assert string_para_inteiro(arquivo_teste, 1) == 1
+  assert string_para_inteiro(arquivo_teste, 2) == 2
+  assert string_para_inteiro(arquivo_teste, 3) == 3
+  assert string_para_inteiro(arquivo_teste, 4) == 4
+  assert string_para_inteiro(arquivo_teste, 5) == 5
