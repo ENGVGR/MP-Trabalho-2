75ee47bcf201f5fb07f37388b66c5c321b9e9af1
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-25 18:42:05 -0300

    Teste para funcao que retorna qual o menor valor

--- testa_juncao.py
+++ testa_juncao.py
@@ -3,7 +3,17 @@ Teste para as funções em juncao.py
 """
 
 from juncao import ler_arquivo
+from juncao import menor_valor
 
-def test_ler_arquivo():
+def teste_ler_arquivo():
   arquivo_teste = ["1\n", "2\n", "3\n", "4\n", "5"]
   assert ler_arquivo("./exemplos/ex1.txt") == arquivo_teste
+
+def teste_menor_valor():
+  assert menor_valor(1, 2) == "Primeiro"
+  assert menor_valor(1, 1) == "Iguais"
+  assert menor_valor(2, 1) == "Segundo"
+  assert menor_valor(4, 6) == "Primeiro"
+  assert menor_valor(8, 2) == "Segundo"
+  assert menor_valor(7, 7) == "Iguais"
+  assert menor_valor(3, 9) == "Primeiro"
